3224d220a8d660c76ebb37b35ff47c5d4295ccb5
Marmowka <dimonshvidun@yandex.ru>
2021-07-31 13:49:37 +0300

    added adaptive header and footer + page.html

--- app/js/main.js
+++ app/js/main.js
@@ -6,12 +6,23 @@ $(function(){
         readOnly: true
     });
 
-    var mixer = mixitup('.products__inner-box');
-
     $('.product-slider__inner').slick({
         dots: true,
         arrows: false,
         slidesToShow: 4,
         slidesToScroll: 4
     });
+
+    $('.menu__btn').on('click', function(){
+        $('.menu__list').slideToggle();
+    });
+
+
+    $('.header__btn-menu').on('click', function(){
+        $('.header__box').toggleClass('active');
+    });
+    
+
+    var mixer = mixitup('.products__inner-box');
+
 });
